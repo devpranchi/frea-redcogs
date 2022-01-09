b59040cdabc0398d534a29a54f0734275e6cbff2
Ronish Katariya <55736291+Ronish1122@users.noreply.github.com>
2022-01-09 11:48:43 +0530

    Update __init__.py

--- statcord/__init__.py
+++ statcord/__init__.py
@@ -5,7 +5,7 @@ from redbot.core.bot import Red
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
-from .statcord import StatcordPost
+from .stat import StatcordPost
 
 
 async def setup(bot: Red) -> None:
