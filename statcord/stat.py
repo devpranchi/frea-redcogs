3605be90879dd3831feaceec53d87d0d74b43480
Ronish Katariya <55736291+Ronish1122@users.noreply.github.com>
2022-01-09 11:54:03 +0530

    Update stat.py

--- statcord/stat.py
+++ statcord/stat.py
@@ -6,7 +6,7 @@ import statcord
 class StatcordPost(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.key = "statcord.com-xHXIAkhmKDqOxH5FbKok"
+        self.key = "statcord.com-"
         self.api = statcord.Client(self.bot,self.key)
         self.api.start_loop()
 
